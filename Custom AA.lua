6a5c5e36d7cbb62dcd7fb6ecfd3d4ebb3817bf7e
zeleney30 <44741166+zeleney30@users.noreply.github.com>
2019-11-18 23:58:08 -0500

    Added edge yaw and fake limit. Fixed some visibility to make menu more functional

--- Custom AA.lua
+++ Custom AA.lua
@@ -54,6 +54,7 @@ antiaims.yawjitter, antiaims.yawjitter_slider = ui.reference("AA", "Anti-aimbot 
 antiaims.fakeyaw, antiaims.fakeyaw_slider = ui.reference("AA", "Anti-aimbot angles", "Body yaw")
 antiaims.freestandbodyyaw = ui.reference("AA", "Anti-aimbot angles", "Freestanding body yaw")
 antiaims.lowerbodyyaw = ui.reference("AA", "Anti-aimbot angles", "Lower body yaw target")
+antiaims.fakeyawlimit = ui.reference("AA", "Anti-aimbot angles", "Fake yaw limit")
 antiaims.edgeyaw = ui.reference("AA", "Anti-aimbot angles", "Edge yaw")
 antiaims.freestanding = ui.reference("AA", "Anti-aimbot angles", "Freestanding")
 antiaims.freestanding_duck = ui.reference("AA", "Anti-aimbot angles", "Freestanding ignore duck")
@@ -107,6 +108,8 @@ local standfakeyaw = interface.combobox("Lua", "A", "Stand: Body yaw", "Off", "O
 local standfakeyaw_slider = interface.slider("Lua", "A", "Stand: Body yaw angle", -180, 180, 0, true)
 local standfreestandbodyyaw = interface.checkbox("Lua", "A", "Stand: Freestanding body yaw")
 local standlowerbodyyaw = interface.combobox("Lua", "A", "Stand: Lower body yaw target", "Off", "Sway", "Opposite", "Eye yaw")
+local standfakelimit = interface.slider("Lua", "A", "Stand: Fake yaw limit", 0, 60, 60, true)
+local standedgeyaw = interface.combobox("Lua", "A", "Stand: Edge yaw", "Off", "Static")
 
 local movepitch = interface.combobox("Lua", "A", "Move: Pitch", "Off", "Default", "Up", "Down", "Minimal", "Random")
 local moveyawbase = interface.combobox("Lua", "A", "Move: Yaw base", "Local view", "At targets")
@@ -118,6 +121,8 @@ local movefakeyaw = interface.combobox("Lua", "A", "Move: Body yaw", "Off", "Opp
 local movefakeyaw_slider = interface.slider("Lua", "A", "Move: Body yaw angle", -180, 180, 0, true)
 local movefreestandbodyyaw = interface.checkbox("Lua", "A", "Move: Freestanding body yaw")
 local movelowerbodyyaw = interface.combobox("Lua", "A", "Move: Lower body yaw target", "Off", "Sway", "Opposite", "Eye yaw")
+local movefakelimit = interface.slider("Lua", "A", "Move: Fake yaw limit", 0, 60, 60, true)
+local moveedgeyaw = interface.combobox("Lua", "A", "Move: Edge yaw", "Off", "Static")
 
 local jumppitch = interface.combobox("Lua", "A", "Jump: Pitch", "Off", "Default", "Up", "Down", "Minimal", "Random")
 local jumpyawbase = interface.combobox("Lua", "A", "Jump: Yaw base", "Local view", "At targets")
@@ -129,6 +134,8 @@ local jumpfakeyaw = interface.combobox("Lua", "A", "Jump: Body yaw", "Off", "Opp
 local jumpfakeyaw_slider = interface.slider("Lua", "A", "Jump: Body yaw angle", -180, 180, 0, true)
 local jumpfreestandbodyyaw = interface.checkbox("Lua", "A", "Jump: Freestanding body yaw")
 local jumplowerbodyyaw = interface.combobox("Lua", "A", "Jump: Lower body yaw target", "Off", "Sway", "Opposite", "Eye yaw")
+local jumpfakelimit = interface.slider("Lua", "A", "Jump: Fake yaw limit", 0, 60, 60, true)
+local jumpedgeyaw = interface.combobox("Lua", "A", "Jump: Edge yaw", "Off", "Static")
 
 local slowmotionpitch = interface.combobox("Lua", "A", "Slow motion: Pitch", "Off", "Default", "Up", "Down", "Minimal", "Random")
 local slowmotionyawbase = interface.combobox("Lua", "A", "Slow motion: Yaw base", "Local view", "At targets")
@@ -140,6 +147,8 @@ local slowmotionfakeyaw = interface.combobox("Lua", "A", "Slow motion: Body yaw"
 local slowmotionfakeyaw_slider = interface.slider("Lua", "A", "Slow motion: Body yaw angle", -180, 180, 0, true)
 local slowmotionfreestandbodyyaw = interface.checkbox("Lua", "A", "Slow motion: Freestanding body yaw")
 local slowmotionlowerbodyyaw = interface.combobox("Lua", "A", "Slow motion: Lower body yaw target", "Off", "Sway", "Opposite", "Eye yaw")
+local slowmotionfakelimit = interface.slider("Lua", "A", "Slow motion: Fake yaw limit", 0, 60, 60, true)
+local slowmotionedgeyaw = interface.combobox("Lua", "A", "Slow motion: Edge yaw", "Off", "Static")
 
 local crouchpitch = interface.combobox("Lua", "A", "Crouch: Pitch", "Off", "Default", "Up", "Down", "Minimal", "Random")
 local crouchyawbase = interface.combobox("Lua", "A", "Crouch: Yaw base", "Local view", "At targets")
@@ -151,6 +160,8 @@ local crouchfakeyaw = interface.combobox("Lua", "A", "Crouch: Body yaw", "Off", 
 local crouchfakeyaw_slider = interface.slider("Lua", "A", "Crouch: Body yaw angle", -180, 180, 0, true)
 local crouchfreestandbodyyaw = interface.checkbox("Lua", "A", "Crouch: Freestanding body yaw")
 local crouchlowerbodyyaw = interface.combobox("Lua", "A", "Crouch: Lower body yaw target", "Off", "Sway", "Opposite", "Eye yaw")
+local crouchfakelimit = interface.slider("Lua", "A", "Crouch: Fake yaw limit", 0, 60, 60, true)
+local crouchedgeyaw = interface.combobox("Lua", "A", "Crouch: Edge yaw", "Off", "Static")
 
 -- AA PRESETS <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
  
@@ -164,6 +175,8 @@ function Standing()
     ui_set(antiaims.yawjitter_slider, ui_get(standyawjitter_slider))
     ui_set(antiaims.fakeyaw, ui_get(standfakeyaw))
     ui_set(antiaims.fakeyaw_slider, ui_get(standfakeyaw_slider))
+    ui_set(antiaims.fakeyawlimit, ui_get(standfakelimit))
+    ui_set(antiaims.edgeyaw, ui_get(standedgeyaw))
     ui_set(antiaims.freestandbodyyaw, ui_get(standfreestandbodyyaw))
     ui_set(antiaims.lowerbodyyaw, ui_get(standlowerbodyyaw))
    
@@ -180,6 +193,8 @@ function Moving()
     ui_set(antiaims.fakeyaw, ui_get(movefakeyaw))
     ui_set(antiaims.fakeyaw_slider, ui_get(movefakeyaw_slider))
     ui_set(antiaims.lowerbodyyaw, ui_get(movelowerbodyyaw))
+    ui_set(antiaims.fakeyawlimit, ui_get(movefakelimit))
+    ui_set(antiaims.edgeyaw, ui_get(moveedgeyaw))
    
 end
  
@@ -194,6 +209,8 @@ function Jumping()
     ui_set(antiaims.fakeyaw, ui_get(jumpfakeyaw))
     ui_set(antiaims.fakeyaw_slider, ui_get(jumpfakeyaw_slider))
     ui_set(antiaims.lowerbodyyaw, ui_get(jumplowerbodyyaw))
+    ui_set(antiaims.fakeyawlimit, ui_get(jumpfakelimit))
+    ui_set(antiaims.edgeyaw, ui_get(jumpedgeyaw))
    
 end
  
@@ -208,6 +225,8 @@ function Slowmotion()
     ui_set(antiaims.fakeyaw, ui_get(slowmotionfakeyaw))
     ui_set(antiaims.fakeyaw_slider, ui_get(slowmotionfakeyaw_slider))
     ui_set(antiaims.lowerbodyyaw, ui_get(slowmotionlowerbodyyaw))
+    ui_set(antiaims.fakeyawlimit, ui_get(slowmotionfakelimit))
+    ui_set(antiaims.edgeyaw, ui_get(slowmotionedgeyaw))
    
 end
  
@@ -222,6 +241,8 @@ function Crouching()
     ui_set(antiaims.fakeyaw, ui_get(crouchfakeyaw))
     ui_set(antiaims.fakeyaw_slider, ui_get(crouchfakeyaw_slider))
     ui_set(antiaims.lowerbodyyaw, ui_get(crouchlowerbodyyaw))
+    ui_set(antiaims.fakeyawlimit, ui_get(slowmotionfakelimit))
+    ui_set(antiaims.edgeyaw, ui_get(slowmotionedgeyaw))
    
 end
  
@@ -305,21 +326,40 @@ local function while_timings()
     end
 end
  
--- DRAW ON SCREEN <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
- 
-local showflags = ui_new_checkbox("Lua", "A", "Display current state")
---local color_picker = ui_new_color_picker("AA", "Other", "Show Flags", 0, 200, 0, 255)
- 
-local function visible()
+-- VISIBILITY <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+local function StandVisibility()
     if ui_get(selector) == "Standing" then
+        if ui_get(standyaw) == "Off" then
+            ui_set_visible(standyawslider, false)
+        else
+            ui_set_visible(standyawslider, true)
+        end
+
+        if ui_get(standyawjitter) == "Off" then
+            ui_set_visible(standyawjitter_slider, false)
+        else
+            ui_set_visible(standyawjitter_slider, true)
+        end
+
+        if ui_get(standfakeyaw) == "Off" or ui_get(standfakeyaw) == "Opposite" then
+            ui_set_visible(standfakeyaw_slider, false)
+        else
+            ui_set_visible(standfakeyaw_slider, true)
+        end
+
+        if ui_get(standlowerbodyyaw) == "Off" or ui_get(standfakeyaw) == "Off" then
+            ui_set_visible(standfakelimit, false)
+        else
+            ui_set_visible(standfakelimit, true)
+        end
+
         ui_set_visible(standpitch, true)
         ui_set_visible(standyawbase, true)
         ui_set_visible(standyaw, true)
-        ui_set_visible(standyawslider, true)
         ui_set_visible(standyawjitter, true)
-        ui_set_visible(standyawjitter_slider, true)
         ui_set_visible(standfakeyaw, true)
-        ui_set_visible(standfakeyaw_slider, true)
+        ui_set_visible(standedgeyaw, true)
         ui_set_visible(standfreestandbodyyaw, true)
         ui_set_visible(standlowerbodyyaw, true)
     else
@@ -331,21 +371,47 @@ local function visible()
         ui_set_visible(standyawjitter_slider, false)
         ui_set_visible(standfakeyaw, false)
         ui_set_visible(standfakeyaw_slider, false)
+        ui_set_visible(standfakelimit, false)
+        ui_set_visible(standedgeyaw, false)
         ui_set_visible(standfreestandbodyyaw, false)
         ui_set_visible(standlowerbodyyaw, false)
     end
+end
 
+local function MoveVisibility()
     if ui_get(selector) == "Moving" then
+        if ui_get(moveyaw) == "Off" then
+            ui_set_visible(moveyawslider, false)
+        else
+            ui_set_visible(moveyawslider, true)
+        end
+
+        if ui_get(moveyawjitter) == "Off" then
+            ui_set_visible(moveyawjitter_slider, false)
+        else
+            ui_set_visible(moveyawjitter_slider, true)
+        end
+
+        if ui_get(movefakeyaw) == "Off" or ui_get(movefakeyaw) == "Opposite" then
+            ui_set_visible(movefakeyaw_slider, false)
+        else
+            ui_set_visible(movefakeyaw_slider, true)
+        end
+
+        if ui_get(movelowerbodyyaw) == "Off" or ui_get(movefakeyaw) == "Off" then
+            ui_set_visible(movefakelimit, false)
+        else
+            ui_set_visible(movefakelimit, true)
+        end
+
         ui_set_visible(movepitch, true)
         ui_set_visible(moveyawbase, true)
         ui_set_visible(moveyaw, true)
-        ui_set_visible(moveyawslider, true)
         ui_set_visible(moveyawjitter, true)
-        ui_set_visible(moveyawjitter_slider, true)
         ui_set_visible(movefakeyaw, true)
-        ui_set_visible(movefakeyaw_slider, true)
         ui_set_visible(movefreestandbodyyaw, true)
         ui_set_visible(movelowerbodyyaw, true)
+        ui_set_visible(moveedgeyaw, true)
     else
         ui_set_visible(movepitch, false)
         ui_set_visible(moveyawbase, false)
@@ -357,19 +423,45 @@ local function visible()
         ui_set_visible(movefakeyaw_slider, false)
         ui_set_visible(movefreestandbodyyaw, false)
         ui_set_visible(movelowerbodyyaw, false)
+        ui_set_visible(movefakelimit, false)
+        ui_set_visible(moveedgeyaw, false)
     end
-        
+end
+
+local function JumpVisibility()
     if ui_get(selector) == "Jumping" then
+        if ui_get(jumpyaw) == "Off" then
+            ui_set_visible(jumpyawslider, false)
+        else
+            ui_set_visible(jumpyawslider, true)
+        end
+
+        if ui_get(jumpyawjitter) == "Off" then
+            ui_set_visible(jumpyawjitter_slider, false)
+        else
+            ui_set_visible(jumpyawjitter_slider, true)
+        end
+
+        if ui_get(jumpfakeyaw) == "Off" or ui_get(jumpfakeyaw) == "Opposite" then
+            ui_set_visible(jumpfakeyaw_slider, false)
+        else
+            ui_set_visible(jumpfakeyaw_slider, true)
+        end
+
+        if ui_get(jumplowerbodyyaw) == "Off" or ui_get(jumpfakeyaw) == "Off" then
+            ui_set_visible(jumpfakelimit, false)
+        else
+            ui_set_visible(jumpfakelimit, true)
+        end
+
         ui_set_visible(jumppitch, true)
         ui_set_visible(jumpyawbase, true)
         ui_set_visible(jumpyaw, true)
-        ui_set_visible(jumpyawslider, true)
         ui_set_visible(jumpyawjitter, true)
-        ui_set_visible(jumpyawjitter_slider, true)
         ui_set_visible(jumpfakeyaw, true)
-        ui_set_visible(jumpfakeyaw_slider, true)
         ui_set_visible(jumpfreestandbodyyaw, true)
         ui_set_visible(jumplowerbodyyaw, true)
+        ui_set_visible(jumpedgeyaw, true)
     else
         ui_set_visible(jumppitch, false)
         ui_set_visible(jumpyawbase, false)
@@ -381,19 +473,45 @@ local function visible()
         ui_set_visible(jumpfakeyaw_slider, false)
         ui_set_visible(jumpfreestandbodyyaw, false)
         ui_set_visible(jumplowerbodyyaw, false)
+        ui_set_visible(jumpfakelimit, false)
+        ui_set_visible(jumpedgeyaw, false)
     end
+end
 
+local function SlowmotionVisibility()
     if ui_get(selector) == "Slow motion" then
+        if ui_get(slowmotionyaw) == "Off" then
+            ui_set_visible(slowmotionyawslider, false)
+        else
+            ui_set_visible(slowmotionyawslider, true)
+        end
+
+        if ui_get(slowmotionyawjitter) == "Off" then
+            ui_set_visible(slowmotionyawjitter_slider, false)
+        else
+            ui_set_visible(slowmotionyawjitter_slider, true)
+        end
+
+        if ui_get(slowmotionfakeyaw) == "Off" or ui_get(slowmotionfakeyaw) == "Opposite" then
+            ui_set_visible(slowmotionfakeyaw_slider, false)
+        else
+            ui_set_visible(slowmotionfakeyaw_slider, true)
+        end
+
+        if ui_get(slowmotionlowerbodyyaw) == "Off" or ui_get(slowmotionfakeyaw) == "Off" then
+            ui_set_visible(slowmotionfakelimit, false)
+        else
+            ui_set_visible(slowmotionfakelimit, true)
+        end
+
         ui_set_visible(slowmotionpitch, true)
         ui_set_visible(slowmotionyawbase, true)
         ui_set_visible(slowmotionyaw, true)
-        ui_set_visible(slowmotionyawslider, true)
         ui_set_visible(slowmotionyawjitter, true)
-        ui_set_visible(slowmotionyawjitter_slider, true)
         ui_set_visible(slowmotionfakeyaw, true)
-        ui_set_visible(slowmotionfakeyaw_slider, true)
         ui_set_visible(slowmotionfreestandbodyyaw, true)
         ui_set_visible(slowmotionlowerbodyyaw, true)
+        ui_set_visible(slowmotionedgeyaw, true)
     else
         ui_set_visible(slowmotionpitch, false)
         ui_set_visible(slowmotionyawbase, false)
@@ -405,19 +523,45 @@ local function visible()
         ui_set_visible(slowmotionfakeyaw_slider, false)
         ui_set_visible(slowmotionfreestandbodyyaw, false)
         ui_set_visible(slowmotionlowerbodyyaw, false)
+        ui_set_visible(slowmotionfakelimit, false)
+        ui_set_visible(slowmotionedgeyaw, false)
     end
+end
 
+local function CrouchVisibility()
     if ui_get(selector) == "Crouching" then
+        if ui_get(crouchyaw) == "Off" then
+            ui_set_visible(crouchyawslider, false)
+        else
+            ui_set_visible(crouchyawslider, true)
+        end
+
+        if ui_get(crouchyawjitter) == "Off" then
+            ui_set_visible(crouchyawjitter_slider, false)
+        else
+            ui_set_visible(crouchyawjitter_slider, true)
+        end
+
+        if ui_get(crouchfakeyaw) == "Off" or ui_get(crouchfakeyaw) == "Opposite" then
+            ui_set_visible(crouchfakeyaw_slider, false)
+        else
+            ui_set_visible(crouchfakeyaw_slider, true)
+        end
+
+        if ui_get(crouchlowerbodyyaw) == "Off" or ui_get(crouchfakeyaw) == "Off" then
+            ui_set_visible(crouchfakelimit, false)
+        else
+            ui_set_visible(crouchfakelimit, true)
+        end
+
         ui_set_visible(crouchpitch, true)
         ui_set_visible(crouchyawbase, true)
         ui_set_visible(crouchyaw, true)
-        ui_set_visible(crouchyawslider, true)
         ui_set_visible(crouchyawjitter, true)
-        ui_set_visible(crouchyawjitter_slider, true)
         ui_set_visible(crouchfakeyaw, true)
-        ui_set_visible(crouchfakeyaw_slider, true)
         ui_set_visible(crouchfreestandbodyyaw, true)
         ui_set_visible(crouchlowerbodyyaw, true)
+        ui_set_visible(crouchedgeyaw, true)
     else
         ui_set_visible(crouchpitch, false)
         ui_set_visible(crouchyawbase, false)
@@ -429,10 +573,15 @@ local function visible()
         ui_set_visible(crouchfakeyaw_slider, false)
         ui_set_visible(crouchfreestandbodyyaw, false)
         ui_set_visible(crouchlowerbodyyaw, false)
+        ui_set_visible(crouchfakelimit, false)
+        ui_set_visible(crouchedgeyaw, false)
     end 
 end
 
-client.set_event_callback('paint', visible)
+-- INDICATOR <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+local showflags = ui_new_checkbox("Lua", "A", "Display current state")
+--local color_picker = ui_new_color_picker("AA", "Other", "Show Flags", 0, 200, 0, 255)
 
 local function on_paint(ctx)
     local screen_width, screen_height = client_screen_size()
@@ -447,4 +596,8 @@ end
  
 client.set_event_callback("run_command", while_timings)
 client.set_event_callback("paint", on_paint)
---client.set_event_callback("paint", get_antiaim)
+client.set_event_callback('paint', StandVisibility)
+client.set_event_callback('paint', MoveVisibility)
+client.set_event_callback('paint', JumpVisibility)
+client.set_event_callback('paint', SlowmotionVisibility)
+client.set_event_callback('paint', CrouchVisibility)
